6a4e042e10e4d8a5a122669fa61515cd450f4472
Panda-the-Pooh <WANG2215@e.ntu.edu.sg>
2024-11-18 08:46:32 +0000

    1

--- app.py
+++ app.py
@@ -6,9 +6,13 @@ from flask import render_template, request
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST']) # front end to back end: post
-
 def index():
     return(render_template('index.html'))
+
+@app.route('/main', methods=['GET', 'POST']) # front end to back end: post
+def main():
+    name = request.form.get('q')
+    return(render_template('main.html'))
 
 if __name__ == '__main__':
     app.run() # app.run(port=1234)
