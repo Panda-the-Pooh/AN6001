fa60f3748daaa10eace032feff6df3d386e94e96
Panda-the-Pooh <WANG2215@e.ntu.edu.sg>
2024-12-09 07:30:54 +0000

    1

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #
 from flask import Flask
 from flask import render_template, request
+import textblob
 
 
 app = Flask(__name__)
@@ -13,6 +14,16 @@ def index():
 def main():
     name = request.form.get('q')
     return(render_template('main.html'))
+
+@app.route('/SA', methods=['GET', 'POST']) # front end to back end: post
+def SA():
+    return(render_template('SA.html'))
+
+@app.route('/SA_result', methods=['GET', 'POST']) # front end to back end: post
+def SA_result():
+    q = request.form.get('q') # former q - backend, latter q - frontend
+    r = textblob.TextBlob(q).sentiment # r - backend
+    return(render_template('SA_result.html', r=r)) # former r - frontend, latter r - backend
 
 if __name__ == '__main__':
     app.run() # app.run(port=1234)
